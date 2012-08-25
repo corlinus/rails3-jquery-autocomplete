e08b99b7cb234565100014fda7ce9c04de51f7e2
Yuriy Esaulov <corlinus@gmail.com>
2012-08-25 14:20:42 +0400

    send attitional data in request

--- lib/rails3-jquery-autocomplete/form_helper.rb
+++ lib/rails3-jquery-autocomplete/form_helper.rb
@@ -35,6 +35,7 @@ module ActionView
     def rewrite_autocomplete_option(options)
       options["data-update-elements"] = JSON.generate(options.delete :update_elements) if options[:update_elements]
       options["data-id-element"] = options.delete :id_element if options[:id_element]
+      options["data-autocomplete-send"] = options.delete :send if options[:send]
       options
     end
   end
